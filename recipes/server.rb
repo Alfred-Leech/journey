0f3668344a5323c750f3ddb30300285d71ccb01c
Alfred Lee <leechenghwa72@gmail.com>
2020-03-08 05:35:56 +0000

    changed default.rb, metadata and index.htm

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,9 +3,15 @@
 # Recipe:: server
 #
 # Copyright (c) 2020 The Authors, All Rights Reserved.
+
+
 package 'httpd'
+
 file '/var/www/html/index.html' do
-  content '<h1> Hello Alfred from AWS !</h1>'
+  content "<h1> Hello Alfred from AWS !</h1>
+<h2>IP ADDR: #{node['ipaddress']}</h2>
+<h2>HOSTNAME: #{node['hostnamectl']['static_hostname']}</h2>
+"
 end
 service 'httpd' do
   action [:enable, :start]
